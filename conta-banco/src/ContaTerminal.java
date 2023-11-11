aea9e44a72f5802fc087246d20e8819ea2156ca4
Cauan Barbaglio <127343662+Cauanvmb@users.noreply.github.com>
2023-11-11 20:40:29 -0300

    Finalizado

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,36 @@
+/*
+ * 
+Numero	        Inteiro	        1021
+Agencia	        Texto	        067-8
+Nome Cliente	Texto	        MARIO ANDRADE
+Saldo	        Decimal	        237.48
+ */
+
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+            
+        Scanner sc = new Scanner(System.in);
+
+        int numero;
+        String agencia;
+        String nomeCliente;
+        float saldo;
+
+        System.out.println("Por favor Insira o seu número de Conta: ");
+        numero = sc.nextInt();
+        sc.nextLine();
+        System.out.println("Por favor Insira o seu número de Agência:");
+        agencia = sc.nextLine();
+
+        System.out.println("Por favor Insira o seu nome:");
+        nomeCliente =sc.nextLine();
+
+        System.out.println("Por favor Insira o seu Saldo:");
+        saldo = sc.nextFloat();
+        
+        System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %.2f já está disponível para saque.", nomeCliente, agencia, numero, saldo);
+        sc.close();
     }
 }
