5b42b50d58ec8ce8b62509d6184cb84e06fda0b0
cauanvmb <cauanvmb15@gmail.com>
2023-12-07 13:57:47 -0300

    remove(), add() and add( by index);

--- duplamente-encadeada/src/one/dio/DoublyLinkedList.java
+++ duplamente-encadeada/src/one/dio/DoublyLinkedList.java
@@ -32,12 +32,54 @@ public class DoublyLinkedList<T> {
      
     public void add(T element){
         DoublyLinkedNode<T> newNode = new DoublyLinkedNode<>(element);
-            firstNode.setnextNode(null);
+        newNode.setnextNode(null);
+        newNode.setpreviousNode(lastNode);
+        if(firstNode == null){
+            firstNode=newNode;
+        if(lastNode != null)
+            lastNode.setnextNode(newNode);
+        }
+            lastNode = newNode;
+            listSize++;
+    }
 
-        if(firstNode.equals(null)){
-            firstNode.setnextNode(newNode);
-            lastNode.setpreviousNode(newNode);
+        public void add(int index, T element){
+
+            DoublyLinkedNode<T> newNode = new DoublyLinkedNode<>(element);
+            DoublyLinkedNode<T> auxNode = getNode(index);
+            newNode.setnextNode(auxNode);
+               
+            if (newNode.getnextNode()!= null){
+                newNode.setpreviousNode(auxNode.getpreviousNode());
+                newNode.getnextNode().setpreviousNode(newNode);
+            }else{
+                lastNode = newNode;
+            }            
+            if(index == 0){
+                firstNode = newNode;
+            }else{
+                newNode.getpreviousNode().setnextNode(newNode);
+            }
+
+            listSize++;
         }
 
-    }
+        public void remove(int index){
+            if(index == 0){
+                firstNode = firstNode.getnextNode();
+                if(firstNode!=null){
+                    firstNode.setpreviousNode(null);
+                }
+            }else{
+                DoublyLinkedNode<T> auxNode = getNode(index);
+                auxNode.getpreviousNode().setnextNode(auxNode.getnextNode());
+                if(auxNode != lastNode){
+                auxNode.getnextNode().setpreviousNode(auxNode.getpreviousNode());
+                }else{
+                    auxNode = lastNode;
+                }
+            }
+            listSize--;
+        }
+
 }
